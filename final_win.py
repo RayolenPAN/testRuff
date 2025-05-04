828e8378ba423fb07c8e9bea2a580004a37c23c0
TlpocTo4eJl <parenb.top.228.8@gmail.com>
2025-05-04 17:52:56 +0500

    23ехМОДЫВТ.лЩ

--- final_win.py
+++ final_win.py
@@ -1,1 +1,21 @@
-# напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.set_appear()
+        self.show()
+    def initUI(self):
+        self.index = QLabel('Индекс Руфье: 4.8')
+        self.heart = QLabel('Работоспособность сердца: выше среднего')
+        self.v_line = QVBoxLayout()
+        self.v_line.addWidget(index,alignment=Qt.AlignCenter)
+        self.v_line.addWidget(heart,alignment=Qt.AlignCenter)
+        self.layout_main = QHBoxLayout()
+        self.layout_main.addLayout(v_line)
+        self.my_wid.setLayout(layout_main)
+    def set_appear(self):
+        self.my_wid.setWindowTitle('Второй экран')
+        self.my_wid.move(900,70)
+        self.my_wid.resize(400,200)
